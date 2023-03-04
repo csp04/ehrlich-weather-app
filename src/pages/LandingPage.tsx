7c3104365c47ead6a8c49ca0e9a062cfbf9ae42e
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 17:08:33 +0800

    fix: update welcome message, use button component

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import { FaGithub } from 'react-icons/fa';
 import { useAuth0 } from '@auth0/auth0-react';
+import Button from '../components/Button';
 
 export default function LandingPage() {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <div className="mt-40 space-y-4">
-      <h2 className="text-4xl font-bold tracking-tight">
-        Ehrlich Weather Forecast
+    <div className="mt-40 space-y-10 flex flex-col">
+      <h2 className="text-2xl font-semibold tracking-tight">
+        Welcome to the weather forecast web application.
+        <br />
+        Please login with your Github user to use the application and view the
+        weather in your city.
       </h2>
       <div className="space-y-4">
-        <button
-          className="border border-gray-400
-                       px-4 py-1
-                       rounded
-                       flex items-center justify-center
-                       space-x-2"
-          onClick={() => loginWithRedirect()}
-        >
-          <FaGithub /> <span>Login with Github</span>
-        </button>
+        <Button onClick={() => loginWithRedirect()}>
+          <span>Login</span>
+        </Button>
       </div>
     </div>
   );
