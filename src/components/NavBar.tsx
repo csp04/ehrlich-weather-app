d43d616b06d4e712a9a3019d4c6e3e5c90593b30
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 17:23:08 +0800

    fix: update style of title

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -8,10 +8,10 @@ export default function NavBar() {
   const { isAuthenticated, logout } = useAuth0();
 
   return (
-    <div className="flex items-center justify-between w-full font-bold text-lg tracking-tighter">
+    <div className="flex items-center justify-between w-full">
       <div className="flex items-center space-x-2">
         <AiFillCloud size={40} />
-        <h1>Weather Forecast</h1>
+        <h1 className="font-bold text-lg tracking-tighter">Weather Forecast</h1>
       </div>
 
       {isAuthenticated && (
