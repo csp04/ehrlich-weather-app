d91334141b279dd1638065fd4f6cd4520bf810d9
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 18:05:07 +0800

    fix: return data as WeatherModel

--- src/api/weather-api.ts
+++ src/api/weather-api.ts
@@ -1,18 +1,21 @@
 import axios from 'axios';
+import WeatherModel from '../models/weather-model';
 
 const url =
   process.env.REACT_APP_WEATHER_URL ||
-  'http://api.openweathermap.org/data/2.5/forecast/daily';
+  'https://api.openweathermap.org/data/2.5/weather';
 const apikey = process.env.REACT_APP_WEATHER_APIKEY || '<weather api key>';
 
 const weatherApi = {
   getWeather: (cityName: string) =>
-    axios.get(url, {
-      params: {
-        q: cityName,
-        appid: apikey,
-      },
-    }),
+    axios
+      .get(url, {
+        params: {
+          q: cityName,
+          appid: apikey,
+        },
+      })
+      .then((res) => res.data as WeatherModel),
 };
 
 export default weatherApi;
