dbe90a1f45b25655e2ddc5588afeac16c0c637ff
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 17:06:42 +0800

    fix: auth, title, logout button
    Add checking if authenticated.
    Add title 'Weather Forecast'.
    Add logout button.

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,5 +1,24 @@
 import React from 'react';
 
+import { useAuth0 } from '@auth0/auth0-react';
+import Button from './Button';
+import { AiFillCloud } from 'react-icons/ai';
+
 export default function NavBar() {
-  return <div>NavBar</div>;
+  const { isAuthenticated, logout } = useAuth0();
+
+  return (
+    <div className="flex items-center justify-between w-full font-bold text-lg tracking-tighter">
+      <div className="flex items-center space-x-2">
+        <AiFillCloud size={40} />
+        <h1>Weather Forecast</h1>
+      </div>
+
+      {isAuthenticated && (
+        <div>
+          <Button onClick={() => logout()}>Logout</Button>
+        </div>
+      )}
+    </div>
+  );
 }
