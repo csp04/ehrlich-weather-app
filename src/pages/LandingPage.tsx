ae7cc96aac1f73f111d7e3e15b53787e4d037111
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 17:51:13 +0800

    fix: update style, tags
    Change h2 to div for welcome title.
    Wrap 2nd sentence with p tag.

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -6,14 +6,15 @@ export default function LandingPage() {
   const { loginWithRedirect } = useAuth0();
 
   return (
-    <div className="mt-40 space-y-10 flex flex-col">
-      <h2 className="text-2xl font-semibold tracking-tight">
+    <div className="mt-24 mx-4 md:mx-0 space-y-8 flex flex-col">
+      <div className="text-2xl font-semibold tracking-tight">
         Welcome to the weather forecast web application.
-        <br />
-        Please login with your Github user to use the application and view the
-        weather in your city.
-      </h2>
+      </div>
       <div className="space-y-4">
+        <p>
+          Please login with your Github user to use the application and view the
+          weather in your city.
+        </p>
         <Button onClick={() => loginWithRedirect()}>
           <span>Login</span>
         </Button>
