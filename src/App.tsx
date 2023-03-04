0fedfa85153a4acf084702db1e0c951418e6cd0e
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 17:07:24 +0800

    feat: add routing and authentication checking

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
 import React from 'react';
 
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import HomePage from './pages/HomePage';
+import LandingPage from './pages/LandingPage';
+import { useAuth0 } from '@auth0/auth0-react';
+
 function App() {
+  const { isLoading, isAuthenticated } = useAuth0();
+
   return (
-    <div>
-      <header></header>
+    <div className="flex flex-col h-screen">
+      <header className="border-b border-gray-300 h-[50px] flex items-center px-4">
+        <NavBar />
+      </header>
+      <main className="flex-grow container mx-auto px-4">
+        <BrowserRouter>
+          <Routes>
+            <Route
+              path="/"
+              element={isAuthenticated ? <HomePage /> : <LandingPage />}
+            />
+          </Routes>
+        </BrowserRouter>
+      </main>
     </div>
   );
 }
