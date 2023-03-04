9f56355c0b52dc275b6c0c450d135a3ac64b00bc
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 16:28:53 +0800

    feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  onClick: () => void;
+  className?: string;
+}
+
+export default function Button({ children, onClick, className }: ButtonProps) {
+  return (
+    <button
+      className={[
+        `border border-gray-400
+      px-4 py-1
+      rounded
+      flex items-center justify-center
+      space-x-2`,
+        className,
+      ].join(' ')}
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+}
