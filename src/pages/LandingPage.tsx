288b3d60c6fc749c2d1a2dc02d23b53e47af3132
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 15:18:56 +0800

    feat: add auth0 login functionality

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { FaGithub } from 'react-icons/fa';
+import { useAuth0 } from '@auth0/auth0-react';
 
 export default function LandingPage() {
+  const { loginWithRedirect } = useAuth0();
+
   return (
     <div className="mt-40 space-y-4">
       <h2 className="text-4xl font-bold tracking-tight">
@@ -14,6 +17,7 @@ export default function LandingPage() {
                        rounded
                        flex items-center justify-center
                        space-x-2"
+          onClick={() => loginWithRedirect()}
         >
           <FaGithub /> <span>Login with Github</span>
         </button>
