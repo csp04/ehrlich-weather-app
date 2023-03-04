cac3e82b53cdfa25c88af789b543fac7d292ebeb
CHARLIE <CHARLIE@DESKTOP-VGV6LSM>
2023-03-04 19:40:57 +0800

    fix: move table to WeatherDisplay component

--- src/pages/WeatherPage.tsx
+++ src/pages/WeatherPage.tsx
@@ -1,9 +1,9 @@
-import moment from 'moment';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import weatherApi from '../api/weather-api';
 
 import Button from '../components/Button';
+import WeatherDisplay from '../components/WeatherDisplay';
 import WeatherModel from '../models/weather-model';
 
 export default function WeatherPage() {
@@ -22,40 +22,11 @@ export default function WeatherPage() {
     weatherApi.getWeather(city).then(setModel);
   }, [navigate, searchParams]);
 
+  if (!model) return null;
+
   return (
-    <div className="mt-40 flex flex-col items-end space-y-20">
-      <table className="w-full border border-gray-400">
-        <thead className="bg-gray-100">
-          <tr className="text-sm h-[32px]">
-            <th>Date (mm/dd/yyyy)</th>
-            <th>Temp (F)</th>
-            <th className="hidden md:table-cell">Description</th>
-            <th className="hidden md:table-cell">Main</th>
-            <th className="hidden md:table-cell">Pressure</th>
-            <th className="hidden md:table-cell">Humidity</th>
-          </tr>
-        </thead>
-        <tbody>
-          {model && (
-            <tr className="text-center h-20">
-              <td>{moment(model.dt * 1000).format('MM/DD/YYYY')}</td>
-              <td>{model.main.temp}</td>
-              <td className="hidden md:table-cell">
-                {model.weather && model.weather.length > 0
-                  ? model.weather[0].description
-                  : ''}
-              </td>
-              <td className="hidden md:table-cell">
-                {model.weather && model.weather.length > 0
-                  ? model.weather[0].main
-                  : ''}
-              </td>
-              <td className="hidden md:table-cell">{model.main.pressure}</td>
-              <td className="hidden md:table-cell">{model.main.humidity}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
+    <div className="mt-40 flex flex-col items-end space-y-20 animate-fade-in">
+      <WeatherDisplay model={model} />
       <Button onClick={() => navigate('/')}>Back</Button>
     </div>
   );
